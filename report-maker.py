b23ca0f2b23f5f1aac965e710005c42f9e02646e
Kokospojken <rigestal@hotmail.com>
2025-10-01 12:00:37 +0200

    företagsnamn och när data senast uppdaterades

--- report-maker.py
+++ report-maker.py
@@ -1,1 +1,32 @@
-print("hello World")
+# Import the json library so that we can handle json
+import json
+
+# Read json from products.json to the variable data
+data = json.load(open("network_devices.json","r",encoding = "utf-8"))
+
+# Create a variable that holds our whole text report
+report = ""
+
+# Company_name and last updated
+company_name = data["company"]
+report += f"Företag: {company_name}\n"
+report += f"Senast uppdaterad: {data['last_updated']}\n"
+report += "-" * 50 + "\n"
+
+
+# loop through the location list 
+for location in data["locations"]:
+    # add the site/'name' of the location to the report
+    report += "\n" + location["site"] + "\n"
+    # add a list of the host names of the devices 
+    # on the location to the report
+    for device in location["devices"]:
+      report += "  " + device["hostname"] + "\n"
+
+
+         
+
+
+# write the report to text file
+with open('report.txt', 'w', encoding='utf-8') as f:
+    f.write(report) 
